e2ab76b5dbe98cddfbf314b960865313713ec6f3
Itamar Tzadok <itamar@substantialmethods.com>
2015-08-05 20:30:44 -0400

    CONTRIB-5861 - Fixed: The mhaairs block can be added in the dashboard (my moodle) page.

--- db/access.php
+++ db/access.php
@@ -58,13 +58,4 @@ $capabilities = array(
             )
      ),
 
-     'block/mhaairs:myaddinstance' => array(
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => array(
-            'user' => CAP_ALLOW
-        ),
-
-        'clonepermissionsfrom' => 'moodle/my:manageblocks'
-     )
 );
